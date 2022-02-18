9b2bd6b5c4a4353422b1eb2d23225d4a3947552a
mateusfonseca <mateusff8@gmail.com>
2022-02-18 10:18:33 +0000

    changed indo in database connection

--- Team/Mateus/myproject/myapp/database.js
+++ Team/Mateus/myproject/myapp/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql2');
 var conn = mysql.createConnection({
   host: 'localhost', // Replace with your host name
   user: 'root',      // Replace with your database username
-  password: 'metaldagama3',      // Replace with your database password
-  database: 'myapp2' // // Replace with your database Name
+  password: 'password',      // Replace with your database password
+  database: 'myapp' // // Replace with your database Name
 });
 conn.connect(function(err) {
   if (err) throw err;
