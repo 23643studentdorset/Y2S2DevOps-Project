eaf4cd441725820e28670d2f28178d678cdacc5e
23643studentdorset <23643@student.dorset-college.ie>
2022-02-18 15:50:12 +0000

    .

--- Team/Luciano/calendarDB.sql
+++ Team/Luciano/calendarDB.sql
@@ -9,11 +9,14 @@ USE calendarDB;
         PASSWORD_   varchar (10) not null,
         ACCESS_LVL  TINYINT      not null CHECK (ACCESS_LVL IN(1,2,3)),
         primary key (USER_NAME)
-        CONSTRAINT USER_NAME    CHECK (USER_NAME LIKE '%@%.%')
+        CONSTRAINT USER_NAME     CHECK (USER_NAME LIKE '%@%.%')
+       
 
     );
 
     INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
     INSERT INTO USER VALUES ('profesor@gmail.com', 'profesor1', 2);
     INSERT INTO USER VALUES ('student@gmail.com', 'student1', 3);
+    //test
     INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
+    
