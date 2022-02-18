5d3bd2b1b7070fcf138a86f4295ba54e72d51f62
23643studentdorset <23643@student.dorset-college.ie>
2022-02-18 13:07:58 +0000

    User table script

---
+++ Team/Luciano/calendarDB.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS calendarDB;
+
+CREATE DATABASE calendarDB;
+
+USE calendarDB; 
+
+    CREATE TABLE IF NOT EXISTS USER (
+        USER_NAME   varchar (25) not null,
+        PASSWORD_   varchar (10) not null,
+        ACCESS_LVL  TINYINT      not null CHECK (ACCESS_LVL IN(1,2,3)),
+        primary key (USER_NAME)
+        CONSTRAINT USER_NAME    CHECK (USER_NAME LIKE '%@%.%')
+
+    );
+
+    INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
+    INSERT INTO USER VALUES ('profesor@gmail.com', 'profesor1', 2);
+    INSERT INTO USER VALUES ('student@gmail.com', 'student1', 3);
+    INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
