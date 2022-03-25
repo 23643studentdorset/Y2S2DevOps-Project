29efeb5587123083cde80b765dee0702abb42ff3
23643studentdorset <23643@student.dorset-college.ie>
2022-03-25 11:41:03 +0000

    Login table

--- Team/Luciano/calendarDB.sql
+++ Team/Luciano/calendarDB.sql
@@ -4,11 +4,11 @@ CREATE DATABASE calendarDB;
 
 USE calendarDB; 
 
-    CREATE TABLE IF NOT EXISTS USER (
+    CREATE TABLE USER (
         USER_NAME   varchar (25) not null,
         PASSWORD_   varchar (10) not null,
         ACCESS_LVL  TINYINT      not null CHECK (ACCESS_LVL IN(1,2,3)),
-        primary key (USER_NAME)
+        primary key (USER_NAME),
         CONSTRAINT USER_NAME     CHECK (USER_NAME LIKE '%@%.%')
        
 
@@ -18,5 +18,7 @@ USE calendarDB;
     INSERT INTO USER VALUES ('profesor@gmail.com', 'profesor1', 2);
     INSERT INTO USER VALUES ('student@gmail.com', 'student1', 3);
     //test
+    INSERT INTO USER VALUES ('student', '123', 2);
     INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
+
     
