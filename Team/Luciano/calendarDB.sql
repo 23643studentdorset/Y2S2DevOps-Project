0128a752af7363c849454d910e4be8697d5c79b9
Luciano <23643@student.dorset-college.ie>
2022-04-01 11:46:30 +0100

    .

--- Team/Luciano/calendarDB.sql
+++ Team/Luciano/calendarDB.sql
@@ -4,21 +4,38 @@ CREATE DATABASE calendarDB;
 
 USE calendarDB; 
 
-    CREATE TABLE USER (
-        USER_NAME   varchar (25) not null,
-        PASSWORD_   varchar (10) not null,
+    CREATE TABLE user (
+        user_name_   VARCHAR (25) not null,
+        password_   VARCHAR (10) not null,
         --ACCESS_LVL: 1 ADMIN / 2 LECTURER / 3 STUDENT
-        ACCESS_LVL  TINYINT      not null CHECK (ACCESS_LVL IN(1,2,3)),
-        primary key (USER_NAME),
-        CONSTRAINT USER_NAME     CHECK (USER_NAME LIKE '%@%.%'));
+        access_lvl  TINYINT      not null CHECK (access_lvl IN(1,2,3)),
+        PRIMARY KEY (user_name_),
+        CONSTRAINT user_name_     CHECK (user_name_ LIKE '%@%.%'));
+
+    
+    
+    INSERT INTO user VALUES ('admin@gmail.com', 'admin1', 1);
+    INSERT INTO user VALUES ('profesor@gmail.com', 'profesor1', 2);
+    INSERT INTO user VALUES ('student@gmail.com', 'student1', 3);
+    --Testing constraints
+    --INSERT INTO USER VALUES ('student', '123', 2);
+    --INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
+
+    CREATE TABLE class (
+        class_id   INT AUTO_INCREMENT PRIMARY KEY,
+        user_name_   VARCHAR (25),
+        lecture_id  INT,
+        PRIMARY KEY (class_id),
+        FOREIGN KEY (user_name_)
+            REFERENCES USER (user_name_)
+        FOREIGN KEY (lecture_id)
+            REFERENCES USER (lectures)
+    );
 
     
     
     INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
     INSERT INTO USER VALUES ('profesor@gmail.com', 'profesor1', 2);
     INSERT INTO USER VALUES ('student@gmail.com', 'student1', 3);
-    --Testing constraints
-    --INSERT INTO USER VALUES ('student', '123', 2);
-    --INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
 
     
