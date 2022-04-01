d0da03af94ece5d6ca57714fd6bfd9133085de4f
Luciano <23643@student.dorset-college.ie>
2022-04-01 11:21:09 +0100

    Readme changed

--- Team/Luciano/calendarDB.sql
+++ Team/Luciano/calendarDB.sql
@@ -4,23 +4,21 @@ CREATE DATABASE calendarDB;
 
 USE calendarDB; 
 
-    CREATE TABLE USER (
+    CREATE TABLE IF NOT EXISTS USER (
         USER_NAME   varchar (25) not null,
         PASSWORD_   varchar (10) not null,
+        --ACCESS_LVL: 1 ADMIN / 2 LECTURER / 3 STUDENT
         ACCESS_LVL  TINYINT      not null CHECK (ACCESS_LVL IN(1,2,3)),
         primary key (USER_NAME),
-        CONSTRAINT USER_NAME     CHECK (USER_NAME LIKE '%@%.%')
-       
+        CONSTRAINT USER_NAME     CHECK (USER_NAME LIKE '%@%.%'));
 
-    );
-
-    //Email - Username - Leve of the Username
-    //LEVEL: 1 ADMIN / 2 Lecture / 3 Student
+    
+    
     INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
     INSERT INTO USER VALUES ('profesor@gmail.com', 'profesor1', 2);
     INSERT INTO USER VALUES ('student@gmail.com', 'student1', 3);
-    //test
-    INSERT INTO USER VALUES ('student', '123', 2);
-    INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
+    --Testing constraints
+    --INSERT INTO USER VALUES ('student', '123', 2);
+    --INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 6);
 
     
