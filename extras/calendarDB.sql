176dd033fa4dbf2a67a4d8214a19d4d51978fe53
23643studentdorset <23643@student.dorset-college.ie>
2022-04-08 11:47:28 +0100

    script working?

--- extras/calendarDB.sql
+++ extras/calendarDB.sql
@@ -7,7 +7,7 @@ USE calendarDB;
     CREATE TABLE user (
         user_name_   VARCHAR (25) not null,
         password_   VARCHAR (10) not null,
-        --ACCESS_LVL: 1 ADMIN / 2 LECTURER / 3 STUDENT
+        -- ACCESS_LVL: 1 ADMIN / 2 LECTURER / 3 STUDENT
         access_lvl  TINYINT      not null CHECK (access_lvl IN(1,2,3)),
         PRIMARY KEY (user_name_),
         CONSTRAINT user_name_     CHECK (user_name_ LIKE '%@%.%'));
@@ -17,9 +17,9 @@ USE calendarDB;
     INSERT INTO user VALUES ('admin@gmail.com', 'admin1', 1);
     INSERT INTO user VALUES ('professor@gmail.com', 'professor1', 2);
     INSERT INTO user VALUES ('student@gmail.com', 'student1', 3);
-    --Testing constraints
-    --INSERT INTO USER VALUES ('student', '123', 2);
-    --INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
+    -- Testing constraints
+    -- INSERT INTO USER VALUES ('student', '123', 2);
+    -- INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
 
     CREATE TABLE class (
         class_id   INT AUTO_INCREMENT PRIMARY KEY,
