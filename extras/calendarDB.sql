c03020d6bdb901ab44b93f4a2802ec0cf1748742
23643studentdorset <23643@student.dorset-college.ie>
2022-04-22 11:20:18 +0100

    .

--- extras/calendarDB.sql
+++ extras/calendarDB.sql
@@ -4,7 +4,7 @@ CREATE DATABASE calendarDB;
 
 USE calendarDB; 
 
-    CREATE TABLE IF NOT EXISTS user(
+    CREATE TABLE user(
         user_name_   VARCHAR (25) NOT NULL,
         password_   VARCHAR (10) NOT NULL,
         -- ACCESS_LVL: 1 ADMIN / 2 LECTURER / 3 STUDENT
@@ -22,16 +22,17 @@ USE calendarDB;
     -- INSERT INTO user VALUES ('admin@gmail.com', 'admin1', 1);
 
      CREATE TABLE lecture(
-        lecture_id   INT AUTO_INCREMENT PRIMARY KEY,
+        lecture_id   INT AUTO_INCREMENT,
         name_  VARCHAR (25),
         carrer  VARCHAR (25),
         year_  VARCHAR (25),
+        PRIMARY KEY (lecture_id)
     );
 
     INSERT INTO lecture (name_, carrer, year_) VALUES ("DevOps", "Bachelor Degree in computer Science", "second year")
     
     CREATE TABLE class(
-        class_id   INT AUTO_INCREMENT PRIMARY KEY,
+        class_id   INT NOT NULL AUTO_INCREMENT,
         user_name_   VARCHAR (25),
         lecture_id  INT,
         PRIMARY KEY (class_id),
@@ -41,6 +42,16 @@ USE calendarDB;
             REFERENCES lecture (lecture_id)
     );
 
+     CREATE TABLE changes (
+        change_id INT NOT NULL AUTO_INCREMENT,
+        user_name_ VARCHAR(25),
+        date_changed DATE,
+        PRIMARY KEY (change_id)
+        FOREIGN KEY (user_name_) REFERENCES user (user_name_)
+        );
+    
+    
+    
     CREATE TABLE event (
         idevent INT NOT NULL AUTO_INCREMENT,
         event_start_week INT NULL,
@@ -55,13 +66,7 @@ USE calendarDB;
 
     INSERT INTO event (event_starts, event_ends, event_title, event_description) VALUES ('2', '3', 'Event test', 'descriptio of the event');
 
-    CREATE TABLE changes (
-        change_id INT NOT NULL AUTO_INCREMENT,
-        user_name_ VARCHAR(25),
-        date_changed DATE,
-        PRIMARY KEY (change_id)
-        FOREIGN KEY (user_name_) REFERENCES user (user_name_)
-        );
+   
 
 
     
