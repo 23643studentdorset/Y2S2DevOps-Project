5857748241016d670ad8456151ebbf521d0861d0
23643studentdorset <23643@student.dorset-college.ie>
2022-04-22 11:03:17 +0100

    lecture table

--- extras/calendarDB.sql
+++ extras/calendarDB.sql
@@ -21,32 +21,35 @@ USE calendarDB;
     -- INSERT INTO USER VALUES ('student', '123', 2);
     -- INSERT INTO USER VALUES ('admin@gmail.com', 'admin1', 1);
 
+     CREATE TABLE lecture(
+        lecture_id   INT AUTO_INCREMENT PRIMARY KEY,
+        name_  VARCHAR (25),
+        carrer  VARCHAR (25),
+        year_  VARCHAR (25),
+    );
+    
     CREATE TABLE class(
         class_id   INT AUTO_INCREMENT PRIMARY KEY,
         user_name_   VARCHAR (25),
         lecture_id  INT,
         PRIMARY KEY (class_id),
         FOREIGN KEY (user_name_)
-            REFERENCES USER (user_name_)
+            REFERENCES USER (user_name_),
         FOREIGN KEY (lecture_id)
-            REFERENCES USER (lectures)
+            REFERENCES lecture (lecture_id)
     );
 
+    CREATE TABLE event (
+        idevent INT NOT NULL AUTO_INCREMENT,
+        event_starts DATETIME NULL,
+        event_ends DATETIME NULL,
+        event_title VARCHAR(255) NULL,
+        event_description LONGTEXT NULL,
+        event_colour VARCHAR(45) NULL,
+        PRIMARY KEY (idevent)
+        );
 
-
-
--- Testing constraints
-
-CREATE TABLE event (
-  idevent INT NOT NULL AUTO_INCREMENT,
-  event_starts DATETIME NULL,
-  event_ends DATETIME NULL,
-  event_title VARCHAR(255) NULL,
-  event_description LONGTEXT NULL,
-  event_colour VARCHAR(45) NULL,
-  PRIMARY KEY (idevent));
-
-  INSERT INTO event (event_starts, event_ends, event_title, event_description) VALUES ('2022-05-22 22:21:01', '2022-05-23 22:21:01', 'Event test', 'descriptio of the event');
+    INSERT INTO event (event_starts, event_ends, event_title, event_description) VALUES ('2022-05-22 22:21:01', '2022-05-23 22:21:01', 'Event test', 'descriptio of the event');
 
 
     
