e8774af7e28c1588293da4ce723bf4a9ae509e8f
Itamar Junior <itjunior@gmail.com>
2022-04-22 15:23:36 +0100

    insert option working

--- calendar.php
+++ calendar.php
@@ -3,7 +3,7 @@
 session_start();
 
 //Load environment variables
-//require "config/config.php";
+require "config/config.php";
 require "functions.php";
 
 // Check if the user is logged in, if not then redirect him to login/index page
@@ -17,16 +17,14 @@ $username = $_SESSION["username"];
 $user_level = getUserData($username, 'access_lvl');
 
 //create a new event and insert it into the database
-if(isset($_POST['insert-event'])){
+if (isset($_POST['insert-event'])) {
     $assessment_title = $_POST['assessment-title'];
     $assessment_color = $_POST['assessment-color'];
     $start_week = $_POST['start-week'];
     $end_week = $_POST['end-week'];
     $assessment_description = $_POST['assessment-description'];
 
-    echo $assessment_description;
     insertCalendarData($start_week, $end_week, $assessment_title, $assessment_description, $assessment_color);
-
 }
 ?>
 
@@ -37,7 +35,7 @@ if(isset($_POST['insert-event'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -55,7 +53,7 @@ if(isset($_POST['insert-event'])){
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link">Welcome <?php echo $_SESSION["username"]; ?> | Access level <?PHP echo $user_level;?> <span class="sr-only">(current)</span></a>
+                        <a class="nav-link">Welcome <?php echo $_SESSION["username"]; ?> | Access level <?PHP echo $user_level; ?> <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="logout.php">Logout</a>
@@ -70,50 +68,63 @@ if(isset($_POST['insert-event'])){
         <div id="centerTitle">
         </div>
 
-        <!-- Populate the calendar using javascript -->
         <div id="calendar">
 
-        <?php for ($counter = 1 ; $counter <=52; $counter++){
-            echo '<div class="days" ';
-            if($user_level <= 2 ){ echo 'data-bs-target="#eventAdd" ';}
-            echo 'data-bs-toggle="modal">'.$counter.'';
-            //Show one event if the weeks starts
-            $title = getCalendarData('event_starts',$counter,'event_title');
-            
-            if ($title != ''){
-                echo '<div class="event" data-bs-target="#eventShow'.$counter.'" data-bs-toggle="modal" style="background-color: #58bae4 ; border-top-left-radius: 5px; border-bottom-left-radius: 5px;">'.$title.'</div>';
-            }
-            if($title == null){
-            //show event if the weeks ends
-            $title = getCalendarData('event_ends',$counter,'event_title');
-            if ($title != ''){
-                echo '<div class="event" data-bs-target="#eventShow'.$counter.'" data-bs-toggle="modal" style="background-color: #58bae4 ; border-top-right-radius: 5px ; border-bottom-right-radius: 5px;">'.$title.'</div>';
-            }}
+            <!-- Loop 52 weeks -->
+            <?php for ($counter = 1; $counter <= 52; $counter++) {
 
-            $description = getCalendarData('event_starts',$counter,'event_description');
-            if ($description == null){
-                $description = getCalendarData('event_ends',$counter,'event_description');
-            }
+                //Show eventAdd inside the week if the user has the correct access level
+                echo '<div class="days" ';
+                if ($user_level <= 2) {
+                    echo 'data-bs-target="#eventAdd" ';
+                }
+                echo 'data-bs-toggle="modal">' . $counter . '';
 
-            echo '</div>
-            <div class="modal fade" id="eventShow'.$counter.'" tabindex="-1" aria-labelledby="eventShowLabel" aria-hidden="true">
-            <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="eventShowLabel">'.$title.'</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">'.$description.'</div>
-                <div class="modal-footer">
-                <button type="button" class="btn btn-primary">Delete Event</button>
-                <button type="button" class="btn btn-primary">Edit</button>
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-            </div>
-            </div>';
 
-        };?>
+                //loop events inside the each week to show multiple events
+                $query_events = "SELECT * FROM event WHERE event_starts = $counter OR event_ends = $counter";
+                $result = mysqli_query($link, $query_events);
+                while ($row = mysqli_fetch_array($result)) {
+                    $idevent = $row['idevent'];
+                    $title = getCalendarData('event_starts', $counter, 'event_title', $idevent);
+                    if ($title == null) {
+                        $title = getCalendarData('event_ends', $counter, 'event_title', $idevent);
+                    }
+                    $event_color = getCalendarData('event_starts', $counter, 'event_colour', $idevent);
+                    if ($event_color == null) {
+                        $event_color = getCalendarData('event_ends', $counter, 'event_colour', $idevent);
+                    }
+                    echo '<div class="event" data-bs-target="#eventShow' . $idevent . '" data-bs-toggle="modal" style="background-color: ' . $event_color . ' ;border-radius: 5px">' . $title . '</div>';
+                    $description = getCalendarData('event_starts', $counter, 'event_description', $idevent);
+                    if ($description == null) {
+                        $description = getCalendarData('event_ends', $counter, 'event_description', $idevent);
+                    }
+
+
+                    echo '<div class="modal fade" id="eventShow' . $idevent . '" tabindex="-1" aria-labelledby="eventShowLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <form method="POST">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="eventShowLabel">' . $title . '</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">' . $description . '</div>
+                    <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" name="delete-event">Delete Event ' . $idevent . '</button>
+                    <button type="button" class="btn btn-primary">Edit</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+                        </form>
+                    </div>
+                    </div>';
+                };
+
+
+                echo '
+                </div>';
+            }; ?>
 
 
             <div class="days" data-bs-target="#eventAdd" data-bs-toggle="modal">6
@@ -129,55 +140,55 @@ if(isset($_POST['insert-event'])){
     </div>
 
 
-   
+
 
     <!-- Modal Add Event -->
     <div class="modal fade" id="eventAdd" tabindex="-1" aria-labelledby="eventAddLabel" aria-hidden="true">
         <div class="modal-dialog">
-        <form method="POST">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="eventAddLabel">Create new Event</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <form method="POST">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="eventAddLabel">Create new Event</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group row">
+                            <label class="col-4 col-form-label" for="assessment-title">Assessment Title</label>
+                            <div class="col-8">
+                                <input id="assessment-title" name="assessment-title" placeholder="Assessment Title" type="text" class="form-control" required="required">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-4 col-form-label" for="assessment-title">Assessment Color</label>
+                            <div class="col-8">
+                                <input id="assessment-title" name="assessment-color" type="color" value="#f6b73c" class="form-control" required="required">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-4 col-form-label" for="start-week">Start Week</label>
+                            <div class="col-8">
+                                <input id="start-week" name="start-week" placeholder="Start Week" type="number" class="form-control" required="required">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-4 col-form-label" for="end-week">End Week</label>
+                            <div class="col-8">
+                                <input id="end-week" name="end-week" placeholder="End Week" type="number" class="form-control" required="required">
+                            </div>
+                        </div>
+                        <div class="form-group row">
+                            <label class="col-4 col-form-label" for="assessment-description">Assessment Description</label>
+                            <div class="col-8">
+                                <textarea id="assessment-description" name="assessment-description" cols="40" rows="5" class="form-control" required="required"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary" name="insert-event">Insert and Save</button>
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
                 </div>
-                <div class="modal-body">
-                        <div class="form-group row">
-                            <label class="col-4 col-form-label" for="assessment-title">Assessment Title</label> 
-                            <div class="col-8">
-                            <input id="assessment-title" name="assessment-title" placeholder="Assessment Title" type="text" class="form-control" required="required">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-4 col-form-label" for="assessment-title">Assessment Color</label> 
-                            <div class="col-8">
-                            <input id="assessment-title" name="assessment-color" type="color" value="#f6b73c" class="form-control" required="required">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-4 col-form-label" for="start-week">Start Week</label> 
-                            <div class="col-8">
-                            <input id="start-week" name="start-week" placeholder="Start Week" type="number" class="form-control" required="required">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-4 col-form-label" for="end-week">End Week</label> 
-                            <div class="col-8">
-                            <input id="end-week" name="end-week" placeholder="End Week" type="number" class="form-control" required="required">
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-4 col-form-label" for="assessment-description">Assessment Description</label> 
-                            <div class="col-8">
-                            <textarea id="assessment-description" name="assessment-description" cols="40" rows="5" class="form-control" required="required"></textarea>
-                            </div>
-                        </div> 
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary" name="insert-event">Insert and Save</button>
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </form>
+            </form>
         </div>
     </div>
 
--- functions.php
+++ functions.php
@@ -9,9 +9,9 @@ function getUserData($username,$rowData){
     }
     mysqli_close($link);
 }
-function getCalendarData($columm,$conditional,$returnRowData){
+function getCalendarData($columm,$conditional,$returnRowData,$idevent){
     include "config/config.php";
-    $query = "SELECT * FROM event WHERE ($columm = '$conditional')";
+    $query = "SELECT * FROM event WHERE $columm = '$conditional' AND (idevent = $idevent)";
     $result = mysqli_query($link, $query);
     while ($row = mysqli_fetch_array($result)) {
         return $row[$returnRowData];
@@ -21,23 +21,23 @@ function getCalendarData($columm,$conditional,$returnRowData){
 
 function insertCalendarData($event_starts,$event_ends,$event_title,$event_description, $event_colour){
     include "config/config.php";
-    $query = "INSERT INTO event (event_starts, event_ends, event_title, event_description, event_colour) VALUES ($event_starts, $event_ends, '$event_title', '$event_description', '$event_colour')";
+    $query = "INSERT INTO calendarDB.event (event_starts, event_ends, event_title, event_description, event_colour) VALUES ($event_starts, $event_ends, '$event_title', '$event_description', '$event_colour')";
     $result = mysqli_query($link, $query);
     if ($result) {
-        echo "Successfully inserted";
+        echo '<div class="alert alert-success" role="alert"> Event inserted with success!</div>';
     } else {
         echo "Could not insert record: ". mysqli_error($link); 
     }
     mysqli_close($link);
 }
-function deleteCalendarData($event_id){
+function deleteCalendarData($idevent){
     include "config/config.php";
-    $query = "DELETE FROM event WHERE (idevent = '$event_id')";
+    $query = "DELETE FROM event WHERE (idevent = '$idevent')";
     $result = mysqli_query($link, $query);
     if ($result) {
         return true;
     } else {
-        echo "Could not insert record: ". mysqli_error($link); 
+        echo "Could not delete record: ". mysqli_error($link); 
     }
     mysqli_close($link);
 }
